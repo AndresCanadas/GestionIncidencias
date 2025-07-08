a27683835c6e39a4708e050d2193852dd7f24b09
Andres Cañadas <39269500+AndresCanadas@users.noreply.github.com>
2025-07-08 13:43:35 +0200

    Update ScriptDatosEstados.sql

--- ScriptDatosEstados.sql
+++ ScriptDatosEstados.sql
@@ -4,16 +4,15 @@ SELECT * FROM Estados;
 
 INSERT INTO Empleados (Nombre, Email)
 VALUES 
-  ('Sin asignar', 'sin.asignar@empresa.com')
+  ('Sin asignar', 'sin.asignar@empresa.com'),
   ('Andrés Cañadas', 'andres.canadas@gmail.com'),
   ('Laura Martínez', 'laura.martinez@example.com'),
   ('Carlos Pérez', 'carlos.perez@example.com');
 
-INSERT INTO Incidencias (IncidenciaID, Titulo, Descripcion, FechaCreacion, EstadoID, EmpleadoID)
+INSERT INTO Incidencias (Titulo, Descripcion, FechaCreacion, EstadoID, EmpleadoID)
 VALUES 
-  (1, 'Pantalla azul', 'El PC se reinicia constantemente con error de pantalla azul.',GETDATE(), 1, 1),
-  (2, 'Problemas de red', 'Sin acceso a Internet en el segundo piso.', DATEADD(DAY, -3, GETDATE()), 2, 2),
-  (3, 'No imprime', 'La impresora de administración no responde.', DATEADD(DAY, -10, GETDATE()), 1, 1),
-  (4, 'Software caído', 'El sistema de gestión dejó de funcionar.', DATEADD(DAY, -1, GETDATE()), 1, NULL);
-
+  ('Pantalla azul', 'El PC se reinicia constantemente con error de pantalla azul.',GETDATE(), 1, 1),
+  ('Problemas de red', 'Sin acceso a Internet en el segundo piso.', DATEADD(DAY, -3, GETDATE()), 2, 2),
+  ('No imprime', 'La impresora de administración no responde.', DATEADD(DAY, -10, GETDATE()), 1, 1),
+  ('Software caído', 'El sistema de gestión dejó de funcionar.', DATEADD(DAY, -1, GETDATE()), 1, NULL);
 
